3a357fa5e9c560ed89c1c3c3d6c106bc5093507b
ilnitskaia-d <ilnitskaia_d@auca.kg>
2026-01-06 15:44:02 +0600

    Started the project. Prepared layouts and packages with activity and fragments.

---
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        val navVersion = "2.7.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.12.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
 }
