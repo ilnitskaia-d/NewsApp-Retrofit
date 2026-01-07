0a3243c16cbb49425b083d32dd30641a804309c6
ilnitskaia-d <ilnitskaia_d@auca.kg>
2026-01-07 15:03:54 +0600

    Prepared Retrofit and API

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "348217390bb84f08adc2c6b64acb9ba7"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
