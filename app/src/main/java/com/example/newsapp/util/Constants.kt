6e5b05f82e4516d47558f50dd6a1101a9fe6c3b6
ilnitskaia-d <ilnitskaia_d@auca.kg>
2026-01-13 13:01:53 +0600

    Set up ArticleFragment.kt and HeadlinesFragment.kt

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
         const val API_KEY = "348217390bb84f08adc2c6b64acb9ba7"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val QUERY_PAGE_SIZE = 20
     }
 }
