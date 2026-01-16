4cc8464c5c2477cd39116e8e81addfc0444ac2c4
ilnitskaia-d <ilnitskaia_d@auca.kg>
2026-01-16 09:39:09 +0600

    FavoritesFragment.kt optimized imports; Fragments added layout specification; Setup SearchFragment.kt; Constants.kt edited DELAY to long

--- app/src/main/java/com/example/newsapp/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/FavoritesFragment.kt
@@ -1,11 +1,8 @@
 package com.example.newsapp.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -18,7 +15,7 @@ import com.example.newsapp.ui.NewsActivity
 import com.example.newsapp.ui.NewsViewModel
 import com.google.android.material.snackbar.Snackbar
 
-class FavoritesFragment : Fragment() {
+class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
 
     lateinit var newsViewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
